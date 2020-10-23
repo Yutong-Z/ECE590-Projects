1f8680e02f828dcb3268b8ce5a5108a7aa79a34e
Yutong Zhang <ashlzy_zyt@163.com>
2020-10-23 00:02:32 -0400

    #1 update detectArbitrage

--- Proj3/project3.py
+++ Proj3/project3.py
@@ -18,9 +18,43 @@ from p3tests import *
 detectArbitrage
 """
 def detectArbitrage(adjList, adjMat, tol=1e-15):
-    ##### Your implementation goes here. #####
-    return []
-    ##### Your implementation goes here. #####
+    # Set initial dist and prev
+    for v in adjList:
+        v.dist = math.inf
+        v.prev = None
+    adjList[0].dist = 0
+    
+    # Iterate |V|-1 times
+    for iter in range(0, len(adjList)-1):
+        
+        # Look at each vertex
+        for v in adjList:
+            
+            # Check each neighbor of v
+            # Update predictions and previous vertex
+            for neigh in v.neigh:
+                # Only update id the new value is better
+                if neigh.dist > v.dist + adjMat[v.rank][neigh.rank] + tol:
+                    neigh.dist = v.dist + adjMat[v.rank][neigh.rank]
+                    neigh.prev = v
+    
+    # Iterate One more time to check negative cost cycle
+    changed = []
+    for v in adjList:
+        for neigh in v.neigh:
+            if neigh.dist > v.dist + adjMat[v.rank][neigh.rank] + tol:
+                neigh.dist = v.dist + adjMat[v.rank][neigh.rank]
+                neigh.prev = v
+                changed.append(neigh)
+    
+    # trace back to find the arbitrage path
+    # remove any vertices that are not part of the cycle (HOW???)
+    v = changed[0] # randomly select a changed vertex???
+    path = []
+    while v.prev != changed[0]:
+        path.append(v)
+    path.reverse()
+    return path
 
 ################################################################################
 
