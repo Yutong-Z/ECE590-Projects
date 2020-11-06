b39b8128b185d0d924b44fef4bd852732fe47fe1
Yutong Zhang <ashlzy_zyt@163.com>
2020-11-06 17:38:49 -0500

    update tracing back procedure

--- Proj4/project4.py
+++ Proj4/project4.py
@@ -15,6 +15,9 @@ from p4tests import *
 
 """
 ED: the edit distance function
+Inputs:
+    src: the original string
+    dest: the string to convert to
 """
 def ED(src, dest):
 
@@ -26,55 +29,48 @@ def ED(src, dest):
     #Initialize the DP table
     D = [[0 for x in range(0,N+1)] for x in range(0,M+1)]
     
-    #Initialize the movement table
-    move = [[0 for x in range(0,N+1)] for x in range(0,M+1)]
-    
-    #Set the values of first row and first column as index number
+    #Base case: set the values of first row and first column as index number
     for i in range(M+1):
         D[i][0] = i
-        move[i][0] = i
     for j in range(N+1):
         D[0][j] = j
-        move[0][j] = j
     
     
     for i in range(1, M+1):
         for j in range(1,N+1):
-            #If the two comparing symbols are the same, there's 
-            #no operation.
+            #If the two comparing symbols are the same,
+            #there's no operation.
             if src[i-1] == dest[j-1]:
                 D[i][j] = D[i-1][j-1]
-                move[i][j] = 'D0'
             else:
                 #Update D[i][j] from three previous cases
                 min_value = min(D[i][j-1], D[i-1][j], D[i-1][j-1])
                 D[i][j] = 1 + min_value
-                #Record the movement of the current step
-                if min_value == D[i][j-1]:
-                    move[i][j] = 'L'
-                elif min_value == D[i-1][j]:
-                    move[i][j] = 'U'
-                else:
-                    move[i][j] = 'D1'
-                
+
+# Track the path of edits implemented                
     i = M
     j = N
     while i > 0 and j > 0:
-        #Track the path of edits implemented
-        if move[i][j] == 'D0':
+        if src[i-1] == dest[j-1]: 
+            #Last symbols match, trace bcak to diagonal
             edits.append(('match', str(src[i-1]), i-1))
             i -= 1
             j -= 1
-        elif move[i][j] == 'L':
-            edits.append(('insert', str(dest[j-1]), i))
-            j -= 1
-        elif move[i][j] == 'U':
-            edits.append(('delete', str(src[i-1]), i-1))
-            i -=1
-        else:
-            edits.append(('sub', str(dest[j-1]), i-1))
-            i -= 1
-            j -= 1
+        else: 
+            #Find where the minimum came from and trace back
+            if D[i][j-1] == D[i][j]:
+                #Minimum came from left cell
+                edits.append(('insert', str(dest[j-1]), i))
+                j -= 1
+            elif D[i-1][j] == D[i][j]:
+                #Minimum came from upper cell
+                edits.append(('delete', str(src[i-1]), i-1))
+                i -=1
+            else:
+                #Minimum came from diagonal cell
+                edits.append(('sub', str(dest[j-1]), i-1))
+                i -= 1
+                j -= 1
     #The operations are all in first row
     while j > 0:
         edits.append(('insert', str(dest[j-1]), i))
